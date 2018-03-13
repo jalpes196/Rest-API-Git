0f479628de546cbf4b80b157326fc1ff6cfce0b4
Jalpesh <Jalpesh@gmail.com>
2018-03-13 23:11:21 +0530

    jetty server added with api resource end point.

--- RestAPIDemo/src/main/java/com/restdemo/resource/RestDemo.java
+++ RestAPIDemo/src/main/java/com/restdemo/resource/RestDemo.java
@@ -1,5 +1,21 @@
 package com.restdemo.resource;
 
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("demo")
+
 public class RestDemo {
 
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@RolesAllowed("user")
+	public String getdata() {
+		
+		return "Hello World..!";
+	}
 }
